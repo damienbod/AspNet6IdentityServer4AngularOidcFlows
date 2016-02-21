380bdfcad496e6bac16edd14bfdc3e6ee16df418
damienbod <damien_bod@hotmail.com>
2016-02-21 10:37:42 +0100

    adding the login logic

--- src/Angular2Client/wwwroot/app/services/SecurityService.js
+++ src/Angular2Client/wwwroot/app/services/SecurityService.js
@@ -11,20 +11,85 @@ var core_1 = require('angular2/core');
 var http_1 = require('angular2/http');
 require('rxjs/add/operator/map');
 var app_constants_1 = require('../app.constants');
+var router_1 = require('angular2/router');
 var SecurityService = (function () {
-    function SecurityService(_http, _configuration) {
+    function SecurityService(_http, _configuration, _router) {
         this._http = _http;
         this._configuration = _configuration;
+        this._router = _router;
+        this.IsAuthorized = false;
+        this.HasAdminRole = false;
         this.actionUrl = _configuration.Server + 'api/DataEventRecords/';
         this.headers = new http_1.Headers();
         this.headers.append('Content-Type', 'application/json');
         this.headers.append('Accept', 'application/json');
+        this.storage = localStorage;
     }
     SecurityService.prototype.ResetAuthorizationData = function () {
+        this.store("authorizationData", "");
+        this.IsAuthorized = false;
+        this.HasAdminRole = false;
     };
     SecurityService.prototype.SetAuthorizationData = function (token) {
+        if (this.retrieve("authorizationData") !== "") {
+            this.store("authorizationData", "");
+        }
+        this.store("authorizationData", token);
+        this.IsAuthorized = true;
+        var data = this.getDataFromToken(token);
+        for (var i = 0; i < data.role.length; i++) {
+            if (data.role[i] === "dataEventRecords.admin") {
+                this.HasAdminRole = true;
+            }
+        }
     };
     SecurityService.prototype.Authorize = function () {
+        this.ResetAuthorizationData();
+        console.log("BEGIN Authorize, no auth data");
+        if (window.location.hash) {
+            console.log("AuthorizedController created, has hash");
+            var hash = window.location.hash.substr(1);
+            var result = hash.split('&').reduce(function (result, item) {
+                var parts = item.split('=');
+                result[parts[0]] = parts[1];
+                return result;
+            }, {});
+            var token = "";
+            if (!result.error) {
+                if (result.state !== this.retrieve("authStateControl")) {
+                    console.log("AuthorizedController created. no myautostate");
+                }
+                else {
+                    this.store("authStateControl", "");
+                    console.log("AuthorizedController created. returning access token");
+                    console.log(result);
+                    token = result.access_token;
+                    var data = this.getDataFromToken(token);
+                    console.log(data);
+                }
+            }
+            this.SetAuthorizationData(token);
+            console.log(this.retrieve("authorizationData"));
+            this._router.navigate(['Overviewindex']);
+        }
+        else {
+            console.log("AuthorizedController time to log on");
+            var authorizationUrl = 'https://localhost:44345/connect/authorize';
+            var client_id = 'angularclient';
+            var redirect_uri = 'https://localhost:44347/authorized';
+            var response_type = "token";
+            var scope = "dataEventRecords aReallyCoolScope";
+            var state = Date.now() + "" + Math.random();
+            this.store("authStateControl", state);
+            console.log("AuthorizedController created. adding myautostate: " + this.retrieve("authStateControl"));
+            var url = authorizationUrl + "?" +
+                "client_id=" + encodeURI(client_id) + "&" +
+                "redirect_uri=" + encodeURI(redirect_uri) + "&" +
+                "response_type=" + encodeURI(response_type) + "&" +
+                "scope=" + encodeURI(scope) + "&" +
+                "state=" + encodeURI(state);
+            window.location.href = url;
+        }
     };
     SecurityService.prototype.Logoff = function () {
     };
@@ -52,9 +117,19 @@ var SecurityService = (function () {
         }
         return data;
     };
+    SecurityService.prototype.retrieve = function (key) {
+        var item = this.storage.getItem(key);
+        if (item && item !== 'undefined') {
+            return JSON.parse(this.storage.getItem(key));
+        }
+        return;
+    };
+    SecurityService.prototype.store = function (key, value) {
+        this.storage.setItem(key, JSON.stringify(value));
+    };
     SecurityService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [http_1.Http, app_constants_1.Configuration])
+        __metadata('design:paramtypes', [http_1.Http, app_constants_1.Configuration, router_1.Router])
     ], SecurityService);
     return SecurityService;
 })();
--- src/Angular2Client/wwwroot/app/services/SecurityService.ts
+++ src/Angular2Client/wwwroot/app/services/SecurityService.ts
@@ -3,32 +3,108 @@ import { Http, Response, Headers } from 'angular2/http';
 import 'rxjs/add/operator/map'
 import { Observable } from 'rxjs/Observable';
 import { Configuration } from '../app.constants';
+import {Router} from 'angular2/router';
 
 @Injectable()
 export class SecurityService {
 
     private actionUrl: string;
     private headers: Headers;
+    private storage: any;
 
-    constructor(private _http: Http, private _configuration: Configuration) {
+    constructor(private _http: Http, private _configuration: Configuration, private _router: Router) {
 
         this.actionUrl = _configuration.Server + 'api/DataEventRecords/';
 
         this.headers = new Headers();
         this.headers.append('Content-Type', 'application/json');
         this.headers.append('Accept', 'application/json');
+        this.storage = localStorage;
     }
+
+    public IsAuthorized: boolean = false;
+    public HasAdminRole: boolean = false;
 
     public ResetAuthorizationData() {
+        this.store("authorizationData", "");
 
+        this.IsAuthorized = false;
+        this.HasAdminRole = false;
     }
 
-    public SetAuthorizationData(token:any) {
+    public SetAuthorizationData(token: any) {
+        if (this.retrieve("authorizationData") !== "") {
+            this.store("authorizationData", "");
+        }
 
+        this.store("authorizationData", token);
+        this.IsAuthorized = true;
+
+        var data: any = this.getDataFromToken(token);
+        for (var i = 0; i < data.role.length; i++) {
+            if (data.role[i] === "dataEventRecords.admin") {
+                this.HasAdminRole = true;
+            }
+        }
     }
 
     public Authorize() {
+        this.ResetAuthorizationData();
 
+        console.log("BEGIN Authorize, no auth data");
+        if (window.location.hash) {
+            console.log("AuthorizedController created, has hash");
+            var hash = window.location.hash.substr(1);
+
+            var result: any = hash.split('&').reduce(function (result, item) {
+                var parts = item.split('=');
+                result[parts[0]] = parts[1];
+                return result;
+            }, {});
+
+            var token = "";
+            if (!result.error) {
+                if (result.state !== this.retrieve("authStateControl")) {
+                    console.log("AuthorizedController created. no myautostate");
+                } else {
+                    this.store("authStateControl", "");
+                    console.log("AuthorizedController created. returning access token");
+                    console.log(result);
+
+                    token = result.access_token;
+                    var data = this.getDataFromToken(token);
+                    console.log(data);
+                }
+            }
+
+            this.SetAuthorizationData(token);
+            console.log(this.retrieve("authorizationData"));
+
+            this._router.navigate(['Overviewindex']);
+        }
+        else {
+            console.log("AuthorizedController time to log on");
+
+            var authorizationUrl = 'https://localhost:44345/connect/authorize';
+            var client_id = 'angularclient';
+            var redirect_uri = 'https://localhost:44347/authorized';
+            var response_type = "token";
+            var scope = "dataEventRecords aReallyCoolScope";
+            var state = Date.now() + "" + Math.random();
+
+            this.store("authStateControl", state);
+            console.log("AuthorizedController created. adding myautostate: " + this.retrieve("authStateControl"));
+
+            var url =
+                authorizationUrl + "?" +
+                "client_id=" + encodeURI(client_id) + "&" +
+                "redirect_uri=" + encodeURI(redirect_uri) + "&" +
+                "response_type=" + encodeURI(response_type) + "&" +
+                "scope=" + encodeURI(scope) + "&" +
+                "state=" + encodeURI(state);
+
+            window.location.href = url;
+        }
     }
 
     public Logoff() {
@@ -61,6 +137,20 @@ export class SecurityService {
         }
 
         return data;
+    }
+
+    private retrieve(key: string): any {
+        var item = this.storage.getItem(key);
+
+        if (item && item !== 'undefined') {
+            return JSON.parse(this.storage.getItem(key));
+        }
+
+        return;
+    }
+
+    private store(key: string, value: any) {
+        this.storage.setItem(key, JSON.stringify(value));
     }
 
 }
