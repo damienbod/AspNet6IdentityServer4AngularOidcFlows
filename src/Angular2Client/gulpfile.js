fd134d4a15996a824f128294e698b13bca494214
damienbod <damien_bod@hotmail.com>
2016-02-21 05:43:06 +0100

    init angular2 client

---
+++ src/Angular2Client/gulpfile.js
@@ -0,0 +1,32 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+var paths = {
+    npmSrc: "./node_modules/",
+    libTarget: "./wwwroot/libs/"
+};
+
+var libsToMove = [
+   paths.npmSrc + '/angular2/bundles/angular2-polyfills.js',
+   paths.npmSrc + '/angular2/bundles/router.dev.js',
+   paths.npmSrc + '/angular2/bundles/http.dev.js',
+   paths.npmSrc + '/angular2/bundles/angular2.dev.js',
+
+   paths.npmSrc + '/systemjs/dist/system.js',
+   paths.npmSrc + '/systemjs/dist/system-polyfills.js',
+
+   paths.npmSrc + '/rxjs/bundles/Rx.js',
+   paths.npmSrc + '/es6-shim/es6-shim.min.js',
+   paths.npmSrc + '/reflect-metadata/Reflect.js',
+   paths.npmSrc + '/zone.js/dist/zone.js',
+   paths.npmSrc + '/jquery/dist/jquery.min.js',
+   
+];
+gulp.task('moveToLibs', function () {
+    return gulp.src(libsToMove).pipe(gulp.dest(paths.libTarget));
+});
+
