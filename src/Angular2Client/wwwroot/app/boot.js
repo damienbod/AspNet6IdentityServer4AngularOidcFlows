5e93a1080a95ac49b4a7f63f53c86f972a404aaa
damienbod <damien_bod@hotmail.com>
2016-02-21 21:50:35 +0100

    adding the  SecurityService  to the boot

--- src/Angular2Client/wwwroot/app/boot.js
+++ src/Angular2Client/wwwroot/app/boot.js
@@ -3,9 +3,11 @@ var router_1 = require('angular2/router');
 var http_1 = require('angular2/http');
 var app_component_1 = require('./app.component');
 var app_constants_1 = require('./app.constants');
+var SecurityService_1 = require('./services/SecurityService');
 browser_1.bootstrap(app_component_1.AppComponent, [
     router_1.ROUTER_PROVIDERS,
     http_1.HTTP_PROVIDERS,
-    app_constants_1.Configuration
+    app_constants_1.Configuration,
+    SecurityService_1.SecurityService
 ]);
 //# sourceMappingURL=boot.js.map
