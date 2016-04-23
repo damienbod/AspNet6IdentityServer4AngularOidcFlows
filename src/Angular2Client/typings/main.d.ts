03e37f295bc690433745487e67bb43073edf8351
damienbod <damien_bod@hotmail.com>
2016-04-23 13:41:12 +0200

    updated the angular2 typings

--- src/Angular2Client/typings/main.d.ts
+++ src/Angular2Client/typings/main.d.ts
@@ -1,1 +1,3 @@
-/// <reference path="main\ambient\es6-shim\es6-shim.d.ts" />
+/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="main/ambient/es6-shim/index.d.ts" />
+/// <reference path="main/ambient/jasmine/index.d.ts" />
