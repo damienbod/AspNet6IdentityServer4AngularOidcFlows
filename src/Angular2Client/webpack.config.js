d20b18b9f7e5e870c8bf6455c906a41f7110f053
damienbod <damien_bod@hotmail.com>
2016-05-31 19:38:33 +0200

    updating to webpack

---
+++ src/Angular2Client/webpack.config.js
@@ -0,0 +1,28 @@
+﻿var webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        "vendor": "./wwwroot/app/vendor",
+        "app": "./wwwroot/app/boot"
+    },
+    output: {
+        path: __dirname,
+        filename: "./wwwroot/dist/[name].bundle.js"
+    },
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+          {
+              test: /\.ts/,
+              loaders: ['ts-loader'],
+              exclude: /node_modules/
+          }
+        ]
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./wwwroot/dist/vendor.bundle.js")
+    ]
+}
---
+++ src/Angular2Client/wwwroot/app/vendor.ts
@@ -0,0 +1,25 @@
+﻿// Polyfill(s) for older browsers.
+import 'core-js/client/core';
+
+// Reflect Metadata.
+import 'reflect-metadata';
+// RxJS.
+import 'rxjs';
+// Zone.
+import 'zone.js/dist/zone';
+
+// Angular 2.
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/core';
+import '@angular/http';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/router';
+
+// angular2localization.
+import 'angular2localization/angular2localization';
+
+// Other libraries.
+import 'jquery/src/jquery';
+import 'bootstrap/dist/js/bootstrap';
