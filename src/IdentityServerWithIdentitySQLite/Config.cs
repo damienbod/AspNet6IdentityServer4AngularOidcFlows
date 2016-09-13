fe53d98f134244b9a8c7213048a838e4c3895e37
damienbod <damien_bod@hotmail.com>
2016-09-13 22:47:31 +0200

    code clean up

--- src/IdentityServerWithIdentitySQLite/Config.cs
+++ src/IdentityServerWithIdentitySQLite/Config.cs
@@ -2,9 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 using IdentityServer4.Models;
-using IdentityServer4.Quickstart;
 using System.Collections.Generic;
-using System.Security.Claims;
 
 namespace QuickstartIdentityServer
 {
