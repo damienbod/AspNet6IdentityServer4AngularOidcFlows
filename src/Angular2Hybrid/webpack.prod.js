6b4a702a9755e165ba393e1d3db87f3ad54fc4e8
damienbod <damien_bod@hotmail.com>
2016-09-16 09:16:48 +0200

    adding client build

---
+++ src/Angular2Hybrid/webpack.prod.js
@@ -0,0 +1,114 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+var Autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var helpers = require('./webpack.helpers');
+
+console.log("@@@@@@@@@ USING PRODUCTION @@@@@@@@@@@@@@@");
+
+module.exports = {
+
+    entry: {
+        'polyfills': './angular2App/polyfills.ts',
+        'vendor': './angular2App/vendor.ts',
+        'app': './angular2App/boot.ts' // our angular app
+    },
+
+    output: {
+        path: "./wwwroot/",
+        filename: 'dist/[name].[hash].bundle.js',
+        publicPath: "/"
+    },
+
+    resolve: {
+        extensions: ['', '.ts', '.js', '.json', '.css', '.scss', '.html']
+    },
+
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal',
+        outputPath: path.join(__dirname, 'wwwroot/')
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loader: 'ts',
+                query: {
+                    'ignoreDiagnostics': [
+                        2403, // 2403 -> Subsequent variable declarations
+                        2300, // 2300 -> Duplicate identifier
+                        2374, // 2374 -> Duplicate number index signature
+                        2375, // 2375 -> Duplicate string index signature
+                        2502 // 2502 -> Referenced directly or indirectly
+                    ]
+                },
+                exclude: [/node_modules\/(?!(ng2-.+))/]
+            },
+
+            // copy those assets to output
+            {
+                test: /\.(png|jpg|gif|ico|woff|woff2|ttf|svg|eot)$/,
+                exclude: /node_modules/,
+                loader: "file?name=assets/[name]-[hash:6].[ext]",
+            },
+
+            // Load css files which are required in vendor.ts
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: "style-loader!css-loader"
+            },
+
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loader: 'raw-loader!style-loader!css-loader!sass-loader'
+            },
+
+            {
+                test: /\.html$/,
+                loader: 'raw'
+            }
+        ],
+        noParse: [/.+zone\.js\/dist\/.+/, /.+angular2\/bundles\/.+/, /angular2-polyfills\.js/]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(
+            [
+                './wwwroot/dist',
+                './wwwroot/fonts',
+                './wwwroot/assets'
+            ]
+        ),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new CommonsChunkPlugin({
+            name: ['vendor', 'polyfills']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            inject: 'body',
+            chunksSortMode: helpers.packageSort(['polyfills', 'vendor', 'app']),
+            template: 'angular2App/index.html'
+        }),
+
+        new CopyWebpackPlugin([
+            { from: './angular2App/images/*.*', to: "assets/", flatten: true }
+        ])
+    ]
+};
+
