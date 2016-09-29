94c71c3972639e48c8c16f6d72c42a6646121e22
damienbod <damien_bod@hotmail.com>
2016-09-29 07:32:49 +0200

    Adding webpack build

---
+++ src/ResourceWithIdentityServerWithClient/webpack.config.js
@@ -0,0 +1,8 @@
+/// <binding ProjectOpened='Watch - Development' />
+var environment = (process.env.NODE_ENV || "development").trim();
+
+if (environment === "development") {
+    module.exports = require('./webpack.dev.js');
+} else {
+    module.exports = require('./webpack.prod.js');
+}
