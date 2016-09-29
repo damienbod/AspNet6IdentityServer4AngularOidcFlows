25952fd21de422b7dd4e93bba2aaa1e71f02c346
damienbod <damien_bod@hotmail.com>
2016-09-29 07:17:42 +0200

    Adding fake  resource server

---
+++ src/ResourceWithIdentityServerWithClient/Controllers/DataEventRecordController.cs
@@ -0,0 +1,49 @@
+using ResourceWithIdentityServerWithClient.Model;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+namespace ResourceWithIdentityServerWithClient.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class DataEventRecordsController : Controller
+    {
+        [Authorize("dataEventRecordsUser")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<DataEventRecord> { new DataEventRecord { Id =1, Description= "Fake", Name="myname", Timestamp= DateTime.UtcNow } });
+        }
+
+        [Authorize("dataEventRecordsAdmin")]
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            return Ok(new DataEventRecord { Id = 1, Description = "Fake", Name = "myname", Timestamp = DateTime.UtcNow });
+        }
+
+        [Authorize("dataEventRecordsAdmin")]
+        [HttpPost]
+        public void Post([FromBody]DataEventRecord value)
+        {
+            
+        }
+
+        [Authorize("dataEventRecordsAdmin")]
+        [HttpPut("{id}")]
+        public void Put(long id, [FromBody]DataEventRecord value)
+        {
+            
+        }
+
+        [Authorize("dataEventRecordsAdmin")]
+        [HttpDelete("{id}")]
+        public void Delete(long id)
+        {
+            
+        }
+    }
+}
