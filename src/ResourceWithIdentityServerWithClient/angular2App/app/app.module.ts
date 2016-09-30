2475a38ed52c7b6127f5435ebb039c4ea3738793
damienbod <damien_bod@hotmail.com>
2016-09-30 15:04:05 +0200

    Updating Angular 2 version

--- src/ResourceWithIdentityServerWithClient/angular2App/app/app.module.ts
+++ src/ResourceWithIdentityServerWithClient/angular2App/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -23,7 +22,6 @@ import { DataEventRecordsEditComponent } from './dataeventrecords/dataeventrecor
 @NgModule({
     imports: [
         BrowserModule,
-        CommonModule,
         FormsModule,
         routing,
         HttpModule,
