08c92cb89b2a043a8e9070fb85db050a9cd5f30e
damienbod <damien_bod@hotmail.com>
2016-11-02 16:52:15 +0100

    Angular 2 entry points

---
+++ src/ResourceWithIdentityServerWithClient/angular2App/main-aot.ts
@@ -0,0 +1,11 @@
+﻿// Entry point for AoT compilation.
+export * from './polyfills';
+export * from './vendor';
+
+import { platformBrowser } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
+import { AppModuleNgFactory } from '../aot/angular2App/app/app.module.ngfactory';
+
+enableProdMode();
+
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
