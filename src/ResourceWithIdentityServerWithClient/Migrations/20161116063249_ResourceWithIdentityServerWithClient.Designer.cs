026a54bfc9f197b7cf7a2d5d113a10250f6fe9c1
damienbod <damien_bod@hotmail.com>
2016-11-16 13:29:13 +0100

    updating database migrations

--- src/ResourceWithIdentityServerWithClient/Migrations/20161116063249_ResourceWithIdentityServerWithClient.Designer.cs
+++ src/ResourceWithIdentityServerWithClient/Migrations/20161116063249_ResourceWithIdentityServerWithClient.Designer.cs
@@ -8,9 +8,10 @@ using IdentityServerWithAspNetIdentity.Data;
 namespace IdentityServerWithIdentitySQLite.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161116063249_ResourceWithIdentityServerWithClient")]
+    partial class ResourceWithIdentityServerWithClient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1");
