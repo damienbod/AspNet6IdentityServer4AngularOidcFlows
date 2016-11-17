7ce777a57d6db913a5168e1829147b48a741664d
damienbod <damien_bod@hotmail.com>
2016-11-17 11:35:22 +0100

    Adding user-management component

--- src/ResourceWithIdentityServerWithClient/angular2App/app/user-management/user-management.component.ts
+++ src/ResourceWithIdentityServerWithClient/angular2App/app/user-management/user-management.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { SecurityService } from '../services/SecurityService';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+
+import { UserManagementService } from '../user-management/UserManagementService';
+import { User } from './models/User';
 
 @Component({
     selector: 'user-management',
@@ -8,13 +14,26 @@ import { Component, OnInit } from '@angular/core';
 export class UserManagementComponent implements OnInit {
 
     public message: string;
-    public values: any[];
+    public Users: User[];
 
-    constructor() {
-        this.message = "UserManagementComponent constructor";
+    constructor(
+        private _userManagementService: UserManagementService,
+        public securityService: SecurityService,
+        private _router: Router) {
+        this.message = "user-management";
     }
     
     ngOnInit() {
-        
+        this.getData();
     }
+
+    private getData() {
+        console.log('User Management:getData starting...');
+        this._userManagementService
+            .GetAll()
+            .subscribe(data => this.Users = data,
+            error => this.securityService.HandleError(error),
+            () => console.log('User Management Get all completed'));
+    }
+
 }
