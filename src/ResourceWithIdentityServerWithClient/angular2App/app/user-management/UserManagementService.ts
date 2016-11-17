afe8831f55c415af0331f14a3c6d4fe381962296
damienbod <damien_bod@hotmail.com>
2016-11-17 11:35:50 +0100

    user management service to access the server data

---
+++ src/ResourceWithIdentityServerWithClient/angular2App/app/user-management/UserManagementService.ts
@@ -0,0 +1,47 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Configuration } from '../app.constants';
+import { SecurityService } from '../services/SecurityService';
+import { User } from './models/User';
+
+@Injectable()
+export class UserManagementService {
+
+    private actionUrl: string;
+    private headers: Headers;
+
+    constructor(private _http: Http, private _configuration: Configuration, private _securityService: SecurityService) {
+        this.actionUrl = `${_configuration.Server}/api/UserManagement/`;   
+    }
+
+    private setHeaders() {
+
+        console.log("setHeaders started");
+
+        this.headers = new Headers();
+        this.headers.append('Content-Type', 'application/json');
+        this.headers.append('Accept', 'application/json');
+
+        var token = this._securityService.GetToken();
+        if (token !== "") {
+            let tokenValue = 'Bearer ' + token;
+            console.log("tokenValue:" + tokenValue);
+            this.headers.append('Authorization', tokenValue);
+        }
+    }
+
+    public GetAll = (): Observable<User[]> => {
+        this.setHeaders();
+        let options = new RequestOptions({ headers: this.headers, body: '' });
+
+        return this._http.get(this.actionUrl, options).map(res => res.json());
+    }
+
+    public Update = (id: number, itemToUpdate: any): Observable<Response> => {
+        this.setHeaders();
+        return this._http
+            .put(this.actionUrl + id, JSON.stringify(itemToUpdate), { headers: this.headers });
+    }
+}
