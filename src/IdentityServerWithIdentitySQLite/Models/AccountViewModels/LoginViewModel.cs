eb50439ea4cb060eda5a2d8de00644afcf7e31c7
damienbod <damien_bod@hotmail.com>
2016-12-02 09:50:55 +0100

    migrating the Models to IdentityServer4 rc4

--- src/IdentityServerWithIdentitySQLite/Models/AccountViewModels/LoginViewModel.cs
+++ src/IdentityServerWithIdentitySQLite/Models/AccountViewModels/LoginViewModel.cs
@@ -1,22 +1,20 @@
-﻿using System;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace IdentityServerWithAspNetIdentity.Models.AccountViewModels
+namespace IdentityServer4.Quickstart.UI.Models
 {
-    public class LoginViewModel
+    public class LoginViewModel : LoginInputModel
     {
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        public bool EnableLocalLogin { get; set; }
+        public IEnumerable<ExternalProvider> ExternalProviders { get; set; }
+    }
 
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
+    public class ExternalProvider
+    {
+        public string DisplayName { get; set; }
+        public string AuthenticationScheme { get; set; }
     }
 }
