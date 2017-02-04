3ade59cc3251a2cc12c24b405cc3e5517fd3652a
damienbod <damien_bod@hotmail.com>
2017-02-04 09:34:18 +0100

    code clean up

--- src/Angular2Client/angular2App/main.js
+++
@@ -1,6 +0,0 @@
-export * from './polyfills';
-export * from './vendor';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-platformBrowserDynamic().bootstrapModule(AppModule);
-//# sourceMappingURL=main.js.map
