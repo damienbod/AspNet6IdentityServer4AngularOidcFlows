617066f4d7dc6e77d2e78bf436e27faae87200ff
damienbod <damien_bod@hotmail.com>
2017-03-18 13:06:37 +0100

    removing build files

--- src/ResourceWithIdentityServerWithClient/angularApp/main-aot.js
+++
@@ -1,5 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from '../aot/angularApp/app/app.module.ngfactory';
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
