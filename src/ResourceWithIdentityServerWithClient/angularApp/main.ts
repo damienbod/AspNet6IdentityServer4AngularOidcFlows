9ded1ff2c22db018c6287e2e0e6cae608cc2a661
damienbod <damien_bod@hotmail.com>
2017-03-18 13:11:46 +0100

    dev build fix

--- src/ResourceWithIdentityServerWithClient/angularApp/main.ts
+++ src/ResourceWithIdentityServerWithClient/angularApp/main.ts
@@ -1,6 +1,7 @@
 ﻿// Entry point for JiT compilation.
 declare var System: any;
 
+import './polyfills';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
