5d1da57b5c87887795ec264b82cada3c7bd8396f
damienbod <damien_bod@hotmail.com>
2017-03-18 11:04:21 +0100

    code clean up

---
+++ src/ResourceWithIdentityServerWithClient/angularApp/main.js
@@ -0,0 +1,7 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+if (module.hot) {
+    module.hot.accept();
+}
+platformBrowserDynamic().bootstrapModule(AppModule);
+//# sourceMappingURL=main.js.map
