7b2611eb37dd6c551b8344651a15f927a2a97681
damienbod <damien_bod@hotmail.com>
2017-03-18 11:14:56 +0100

    updating packages

--- src/ResourceWithIdentityServerWithClient/angularApp/main-aot.js
+++ src/ResourceWithIdentityServerWithClient/angularApp/main-aot.js
@@ -1,5 +1,3 @@
-export * from './polyfills';
-export * from './vendor';
 import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from '../aot/angularApp/app/app.module.ngfactory';
