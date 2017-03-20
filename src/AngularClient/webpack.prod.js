bb159dcd469f50bdb428f8e531281bb236053f12
damienbod <damien_bod@hotmail.com>
2017-03-20 07:09:35 +0100

    updating webpack

--- src/AngularClient/webpack.prod.js
+++ src/AngularClient/webpack.prod.js
@@ -97,7 +97,7 @@ module.exports = {
         }),
 
         new CopyWebpackPlugin([
-            { from: './angularApp/images/*.*', to: 'assets/', flatten: true }
+            { from: './angularApp/assets/*.*', to: 'assets/', flatten: true }
         ])
     ]
 };
