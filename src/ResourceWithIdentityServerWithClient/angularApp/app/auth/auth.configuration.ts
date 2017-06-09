0a1236352897a7e1b03681c910dcda1698b51c13
damienbod <damien_bod@hotmail.com>
2017-06-09 16:47:32 +0200

    updating configuration for well known clients

--- src/ResourceWithIdentityServerWithClient/angularApp/app/auth/auth.configuration.ts
+++ src/ResourceWithIdentityServerWithClient/angularApp/app/auth/auth.configuration.ts
@@ -3,23 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthConfiguration {
 
-    // The Issuer Identifier for the OpenID Provider (which is typically obtained during Discovery) MUST exactly match the value of the iss (issuer) Claim.
-    iss = 'https://localhost:44363';
-
-    server = 'https://localhost:44363';
-
     redirect_url = 'https://localhost:44363';
 
-    authorise_url = 'https://localhost:44363/connect/authorize';
-
-    // This is required to get the signing keys so that the signiture of the Jwt can be validated.
-    jwks_url = 'https://localhost:44363/.well-known/openid-configuration/jwks';
-
-    userinfo_url = 'https://localhost:44363/connect/userinfo';
-
-    logoutEndSession_url = 'https://localhost:44363/connect/endsession';
-
-    checksession_url = 'https://localhost:44363/connect/checksession'
+    stsServer = 'https://localhost:44363';
 
     // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
     // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
