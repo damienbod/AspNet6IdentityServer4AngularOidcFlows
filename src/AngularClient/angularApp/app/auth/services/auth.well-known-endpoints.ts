6d3f3bf869bd22b4f2b9bad3c71eda639b957421
damienbod <damien_bod@hotmail.com>
2017-06-13 12:49:18 +0200

    strict type fixes

--- src/AngularClient/angularApp/app/auth/services/auth.well-known-endpoints.ts
+++ src/AngularClient/angularApp/app/auth/services/auth.well-known-endpoints.ts
@@ -88,6 +88,6 @@ export class AuthWellKnownEndpoints {
         return this.http.get(this.authConfiguration.stsServer + '/.well-known/openid-configuration', {
             headers: headers,
             body: ''
-        }).map(res => res.json());
+        }).map((res: any) => res.json());
     }
 }
