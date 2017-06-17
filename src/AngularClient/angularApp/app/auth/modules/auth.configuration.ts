371f59d2a8c5310485187f87b9eaba1559d1dfe1
damienbod <damien_bod@hotmail.com>
2017-06-17 07:43:56 +0200

    init

--- src/AngularClient/angularApp/app/auth/modules/auth.configuration.ts
+++ src/AngularClient/angularApp/app/auth/modules/auth.configuration.ts
@@ -31,9 +31,13 @@ export class AuthConfiguration {
 
     log_console_warning_active = true;
 
-    log_console_debug_active = true;
+    log_console_debug_active = false;
 
     // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
     // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
     max_id_token_iat_offset_allowed_in_seconds = 3;
+
+    override_well_known_configuration = false;
+
+    override_well_known_configuration_url = 'https://localhost:44386/wellknownconfiguration.json';
 }
