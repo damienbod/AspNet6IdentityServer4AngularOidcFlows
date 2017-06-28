b430a69d27798a21bb8fb47480786d3ca7cb3afe
damienbod <damien_bod@hotmail.com>
2017-06-28 17:17:09 +0200

    updating configuration

--- src/AngularClient/angularApp/app/app.module.ts
+++ src/AngularClient/angularApp/app/app.module.ts
@@ -19,8 +19,10 @@ import { SecureFilesComponent } from './securefile/securefiles.component';
 import { DataEventRecordsListComponent } from './dataeventrecords/dataeventrecords-list.component';
 import { DataEventRecordsCreateComponent } from './dataeventrecords/dataeventrecords-create.component';
 import { DataEventRecordsEditComponent } from './dataeventrecords/dataeventrecords-edit.component';
+
 import { AuthModule } from './auth/modules/auth.module';
 import { OidcSecurityService } from './auth/services/oidc.security.service';
+import { OpenIDImplicitFlowConfiguration } from './auth/modules/auth.configuration';
 
 @NgModule({
     imports: [
@@ -50,4 +52,32 @@ import { OidcSecurityService } from './auth/services/oidc.security.service';
     bootstrap:    [AppComponent],
 })
 
-export class AppModule {}
+export class AppModule {
+    constructor(public oidcSecurityService: OidcSecurityService) {
+
+        let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+        openIDImplicitFlowConfiguration.stsServer = 'https://localhost:44318';
+
+        openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44311';
+        // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
+        // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
+        openIDImplicitFlowConfiguration.client_id = 'angularclient';
+        openIDImplicitFlowConfiguration.response_type = 'id_token token';
+        openIDImplicitFlowConfiguration.scope = 'dataEventRecords securedFiles openid';
+        openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44311/Unauthorized';
+        openIDImplicitFlowConfiguration.start_checksession = false;
+        openIDImplicitFlowConfiguration.silent_renew = true;
+        openIDImplicitFlowConfiguration.startup_route = '/dataeventrecords/list';
+        // HTTP 403
+        openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
+        // HTTP 401
+        openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
+        openIDImplicitFlowConfiguration.log_console_warning_active = true;
+        openIDImplicitFlowConfiguration.log_console_debug_active = false;
+        // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
+        // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
+        openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 3;
+
+        this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
+    }
+}
--- src/AngularClientIdTokenOnly/angularApp/app/app.module.ts
+++ src/AngularClientIdTokenOnly/angularApp/app/app.module.ts
@@ -12,6 +12,7 @@ import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
 import { AuthModule } from './auth/modules/auth.module';
 import { OidcSecurityService } from './auth/services/oidc.security.service';
+import { OpenIDImplicitFlowConfiguration } from './auth/modules/auth.configuration';
 
 @NgModule({
     imports: [
@@ -35,4 +36,33 @@ import { OidcSecurityService } from './auth/services/oidc.security.service';
     bootstrap:    [AppComponent],
 })
 
-export class AppModule {}
+
+export class AppModule {
+    constructor(public oidcSecurityService: OidcSecurityService) {
+
+        let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+
+        openIDImplicitFlowConfiguration.stsServer = 'https://localhost:44318';
+        openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44372';
+        // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
+        // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
+        openIDImplicitFlowConfiguration.client_id = 'angularclientidtokenonly';
+        openIDImplicitFlowConfiguration.response_type = 'id_token';
+        openIDImplicitFlowConfiguration.scope = 'openid profile email';
+        openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44372/Unauthorized';
+        openIDImplicitFlowConfiguration.start_checksession = false;
+        openIDImplicitFlowConfiguration.silent_renew = true;
+        openIDImplicitFlowConfiguration.startup_route = '/home';
+        // HTTP 403
+        openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
+        // HTTP 401
+        openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
+        openIDImplicitFlowConfiguration.log_console_warning_active = true;
+        openIDImplicitFlowConfiguration.log_console_debug_active = false;
+        // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
+        // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
+        openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 3;
+
+        this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
+    }
+}
