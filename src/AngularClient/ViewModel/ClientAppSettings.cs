79862ad4085043701551a6535e8fe0249763a42f
damienbod <damien_bod@hotmail.com>
2017-08-12 21:01:43 +0200

    updating types for bool

--- src/AngularClient/ViewModel/ClientAppSettings.cs
+++ src/AngularClient/ViewModel/ClientAppSettings.cs
@@ -8,13 +8,13 @@
         public string response_type { get; set; }
         public string scope { get; set; }
         public string post_logout_redirect_uri { get; set; }
-        public string start_checksession { get; set; }
-        public string silent_renew { get; set; }
+        public bool start_checksession { get; set; }
+        public bool silent_renew { get; set; }
         public string startup_route { get; set; }
         public string forbidden_route { get; set; }
         public string unauthorized_route { get; set; }
-        public string log_console_warning_active { get; set; }
-        public string log_console_debug_active { get; set; }
+        public bool log_console_warning_active { get; set; }
+        public bool log_console_debug_active { get; set; }
         public string max_id_token_iat_offset_allowed_in_seconds { get; set; }
         public string apiServer { get; set; }
         public string apiFileServer { get; set; }
