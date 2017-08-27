530be662bfb57780c5d91f63a060af5e97ce3f08
damienbod <damien_bod@hotmail.com>
2017-08-27 09:07:36 +0200

    update 1.3.12

--- src/AngularClientIdTokenOnly/angularApp/vendor.js
+++
@@ -1,6 +0,0 @@
-import 'jquery/src/jquery';
-import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import '../favicon.ico';
-//# sourceMappingURL=vendor.js.map
