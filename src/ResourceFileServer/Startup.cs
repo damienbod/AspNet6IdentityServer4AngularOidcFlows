2e96aac1fee1815d4c18d4f751f06591cd18e9d0
damienbod <damien_bod@hotmail.com>
2017-09-15 11:48:52 +0200

    Upadting APIs to .NET 2.0

--- src/ResourceFileServer/Startup.cs
+++ src/ResourceFileServer/Startup.cs
@@ -4,14 +4,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using ResourceFileServer.Providers;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ResourceFileServer
 {
-    using System.Collections.Generic;
-    using System.IdentityModel.Tokens.Jwt;
-    using System.IO;
-    using Microsoft.AspNetCore.Mvc.Authorization;
-    using Providers;
     public class Startup
     {
         public Startup(IHostingEnvironment env)
@@ -43,6 +46,53 @@ namespace ResourceFileServer
                 .RequireClaim("scope", "securedFiles")
                 .Build();
 
+            //services.AddAuthentication((options) =>
+            //{
+            //    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //})
+            //.AddJwtBearer(options =>
+            //{
+            //    options.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuer = true, 
+            //        ValidateAudience = true,
+            //        ValidateLifetime = true,
+            //        ValidateIssuerSigningKey = true,
+
+            //        ValidIssuer = "Fiver.Security.Bearer",
+            //        ValidAudience = "Fiver.Security.Bearer",
+            //        IssuerSigningKey = JwtSecurityKey.Create("fiversecret ")
+            //    }; ;
+            //    options.Authority = "https://localhost:44318/";
+            //    options. = new List<string> { "securedFiles" };
+            //    options.ApiName = "securedFiles";
+            //    options.ApiSecret = "securedFilesSecret";
+            //});
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+               .AddIdentityServerAuthentication(options =>
+               {
+                   options.Authority = "https://localhost:44318/";
+                   options.AllowedScopes = new List<string> { "securedFiles" };
+                   options.ApiName = "securedFiles";
+                   options.ApiSecret = "securedFilesSecret";
+               });
+
+            //IdentityServerAuthenticationOptions identityServerAuthenticationOptions = new IdentityServerAuthenticationOptions()
+            //{
+            //    Authority = "https://localhost:44318/",
+            //    AllowedScopes = new List<string> { "securedFiles" },
+            //    ApiSecret = "securedFilesSecret",
+            //    ApiName = "securedFiles",
+            //    AutomaticAuthenticate = true,
+            //    // required if you want to return a 403 and not a 401 for forbidden responses
+            //    AutomaticChallenge = true
+            //};
+            //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            //app.UseIdentityServerAuthentication(identityServerAuthenticationOptions);
+
+
             services.AddAuthorization(options =>
             {
                 options.AddPolicy("securedFilesUser", policyUser =>
@@ -71,19 +121,7 @@ namespace ResourceFileServer
 
             app.UseStaticFiles();
 
-            IdentityServerAuthenticationOptions identityServerAuthenticationOptions = new IdentityServerAuthenticationOptions()
-            {
-                Authority = "https://localhost:44318/",
-                AllowedScopes = new List<string> { "securedFiles" },
-                ApiSecret = "securedFilesSecret",
-                ApiName = "securedFiles",
-                AutomaticAuthenticate = true,
-                // required if you want to return a 403 and not a 401 for forbidden responses
-                AutomaticChallenge = true
-            };
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-            app.UseIdentityServerAuthentication(identityServerAuthenticationOptions);
-
+            app.UseAuthentication();
             app.UseMvc();
         }
 
