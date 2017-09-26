367d10047186bb8d46a05c7ea4969fe1fe68c9ab
damienbod <damien_bod@hotmail.com>
2017-09-26 20:21:37 +0200

    Updating auto module to 1.3.16

--- src/ResourceWithIdentityServerWithClient/angularApp/app/auth/services/oidc.security.service.ts
+++ src/ResourceWithIdentityServerWithClient/angularApp/app/auth/services/oidc.security.service.ts
@@ -72,6 +72,11 @@ export class OidcSecurityService {
 
         if (isPlatformBrowser(this.platformId)) {
             // Client only code.
+            this.authWellKnownEndpoints.onWellKnownEndpointsLoaded.subscribe(() => {
+                this.moduleSetup = true;
+                this.onModuleSetup.emit();
+            });
+
             this.authWellKnownEndpoints.setupModule();
 
             if (this.authConfiguration.silent_renew) {
@@ -84,9 +89,11 @@ export class OidcSecurityService {
                 });
             }
         }
-
-        this.moduleSetup = true;
-        this.onModuleSetup.emit();
+        else
+        {
+            this.moduleSetup = true;
+            this.onModuleSetup.emit();
+        }
     }
 
     getUserData(): Observable<any> {
@@ -99,7 +106,9 @@ export class OidcSecurityService {
     }
 
     getIsAuthorized(): Observable<boolean> {
-        return this._isAuthorized.asObservable();
+		if(this.moduleSetup) {
+            return this._isAuthorized.asObservable();
+		}
     }
 
     private setIsAuthorized(isAuthorized: boolean): void {
@@ -292,6 +301,7 @@ export class OidcSecurityService {
                             }
                         });
                     } else {
+                        this.runTokenValidatation();
                         if (this.authConfiguration.trigger_authorization_result_event) {
 							this.onAuthorizationResult.emit(AuthorizationResult.authorized);
 						} else {
