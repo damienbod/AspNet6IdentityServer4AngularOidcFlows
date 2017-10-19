5e3f32da9da9cc5a2c5a6cf248805e947d8b0820
damienbod <damien_bod@hotmail.com>
2017-10-19 09:07:11 +0200

    build fixes

--- src/AngularClient/angularApp/app/app.module.ts
+++ src/AngularClient/angularApp/app/app.module.ts
@@ -72,7 +72,7 @@ export class AppModule {
             openIDImplicitFlowConfiguration.post_logout_redirect_uri = this.clientConfiguration.post_logout_redirect_uri;
             openIDImplicitFlowConfiguration.start_checksession = this.clientConfiguration.start_checksession;
             openIDImplicitFlowConfiguration.silent_renew = this.clientConfiguration.silent_renew;
-            openIDImplicitFlowConfiguration.startup_route = this.clientConfiguration.startup_route;
+            openIDImplicitFlowConfiguration.login_route = this.clientConfiguration.startup_route;
             // HTTP 403
             openIDImplicitFlowConfiguration.forbidden_route = this.clientConfiguration.forbidden_route;
             // HTTP 401
