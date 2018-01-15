e9175cf28ab3e28ac67f7be5b819e1f9a3315f19
damienbod <damien_bod@hotmail.com>
2018-01-15 08:48:32 +0100

    updating init to use APP_INITIALIZER

--- src/AngularClientIdTokenOnly/angularApp/app/app.module.ts
+++ src/AngularClientIdTokenOnly/angularApp/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -15,6 +15,13 @@ import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 import { AuthModule } from './auth/modules/auth.module';
 import { OidcSecurityService } from './auth/services/oidc.security.service';
 import { OpenIDImplicitFlowConfiguration } from './auth/modules/auth.configuration';
+import { OidcConfigService } from './auth/services/oidc.security.config.service';
+import { AuthWellKnownEndpoints } from './auth/models/auth.well-known-endpoints';
+
+export function loadConfig(oidcConfigService: OidcConfigService) {
+    console.log('APP_INITIALIZER STARTING');
+    return () => oidcConfigService.load_using_stsServer('https://localhost:44318');
+}
 
 @NgModule({
     imports: [
@@ -31,45 +38,61 @@ import { OpenIDImplicitFlowConfiguration } from './auth/modules/auth.configurati
         UnauthorizedComponent
     ],
     providers: [
+        OidcConfigService,
         OidcSecurityService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: loadConfig,
+            deps: [OidcConfigService],
+            multi: true
+        },
         Configuration
     ],
-    bootstrap:    [AppComponent],
+    bootstrap: [AppComponent],
 })
 
 
 export class AppModule {
-    constructor(public oidcSecurityService: OidcSecurityService) {
+    constructor(
+        public oidcSecurityService: OidcSecurityService,
+        public oidcConfigService: OidcConfigService
+    ) {
 
-        const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+        this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+            const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
 
-        const flowType = 'id_token';
+            const flowType = 'id_token';
 
-        // const flowType = 'id_token token';
-        // openIDImplicitFlowConfiguration.auto_userinfo = false;
+            // const flowType = 'id_token token';
+            // openIDImplicitFlowConfiguration.auto_userinfo = false;
 
-        openIDImplicitFlowConfiguration.stsServer = 'https://localhost:44318';
-        openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44372';
-        // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
-        // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
-        openIDImplicitFlowConfiguration.client_id = 'angularclientidtokenonly';
-        openIDImplicitFlowConfiguration.response_type = flowType;
-        openIDImplicitFlowConfiguration.scope = 'openid profile email';
-        openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44372/Unauthorized';
-        openIDImplicitFlowConfiguration.start_checksession = false;
-        openIDImplicitFlowConfiguration.silent_renew = true;
-        openIDImplicitFlowConfiguration.post_login_route = '/home';
-        // HTTP 403
-        openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
-        // HTTP 401
-        openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
-        openIDImplicitFlowConfiguration.log_console_warning_active = true;
-        openIDImplicitFlowConfiguration.log_console_debug_active = false;
-        // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
-        // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
-        openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 3;
-        openIDImplicitFlowConfiguration.auto_clean_state_after_authentication = false;
+            openIDImplicitFlowConfiguration.stsServer = 'https://localhost:44318';
+            openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44372';
+            // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
+            // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
+            openIDImplicitFlowConfiguration.client_id = 'angularclientidtokenonly';
+            openIDImplicitFlowConfiguration.response_type = flowType;
+            openIDImplicitFlowConfiguration.scope = 'openid profile email';
+            openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44372/Unauthorized';
+            openIDImplicitFlowConfiguration.start_checksession = false;
+            openIDImplicitFlowConfiguration.silent_renew = true;
+            openIDImplicitFlowConfiguration.post_login_route = '/home';
+            // HTTP 403
+            openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
+            // HTTP 401
+            openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
+            openIDImplicitFlowConfiguration.log_console_warning_active = true;
+            openIDImplicitFlowConfiguration.log_console_debug_active = false;
+            // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
+            // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
+            openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 3;
+            openIDImplicitFlowConfiguration.auto_clean_state_after_authentication = false;
 
-        this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
+            const authWellKnownEndpoints = new AuthWellKnownEndpoints();
+            authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
+
+            this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
+
+        });
     }
 }
