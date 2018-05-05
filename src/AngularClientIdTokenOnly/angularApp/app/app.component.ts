3dad5af97211288b364d48bfa9b81359a3757d3d
damienbod <damien_bod@hotmail.com>
2018-05-05 17:56:43 +0200

    Updating Angular 6

--- src/AngularClientIdTokenOnly/angularApp/app/app.component.ts
+++ src/AngularClientIdTokenOnly/angularApp/app/app.component.ts
@@ -1,5 +1,5 @@
-﻿import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { OidcSecurityService } from './auth/services/oidc.security.service';
 
 import './app.component.css';
