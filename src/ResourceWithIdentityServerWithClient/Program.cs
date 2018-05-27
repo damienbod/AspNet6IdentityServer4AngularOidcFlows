763dfa0e38c48a46cdd88cac563210ba2e006885
damienbod <damien_bod@hotmail.com>
2018-05-27 18:08:34 +0200

    updating to .NET Core 2.1

--- src/ResourceWithIdentityServerWithClient/Program.cs
+++ src/ResourceWithIdentityServerWithClient/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace IdentityServerWithAspNetIdentitySqlite
@@ -11,17 +7,11 @@ namespace IdentityServerWithAspNetIdentitySqlite
     {
         public static void Main(string[] args)
         {
-            Console.Title = "IdentityServer";
-
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls("http://localhost:5000")
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
