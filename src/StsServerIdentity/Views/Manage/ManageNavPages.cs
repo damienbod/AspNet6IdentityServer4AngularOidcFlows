07ea8bd8f5c53d12841c8872e39d7f0763d5981a
damienbod <damien_bod@hotmail.com>
2018-08-26 13:25:15 +0200

    Adding PERSONAL_DATA to the management profile

--- src/StsServerIdentity/Views/Manage/ManageNavPages.cs
+++ src/StsServerIdentity/Views/Manage/ManageNavPages.cs
@@ -16,6 +16,8 @@ namespace StsServerIdentity.Views.Manage
 
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
+        public static string PersonalData => "PersonalData";
+
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
@@ -23,7 +25,9 @@ namespace StsServerIdentity.Views.Manage
         public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
 
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
-    
+
+        public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
+
         public static string PageNavClass(ViewContext viewContext, string page)
         {
             var activePage = viewContext.ViewData["ActivePage"] as string;
