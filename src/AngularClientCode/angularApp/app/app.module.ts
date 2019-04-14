b0a6349e1cd3bcc0ac30695c57bff91424030775
damienbod <damien_bod@hotmail.com>
2019-04-14 13:23:45 +0200

    code clean up

--- src/AngularClientCode/angularApp/app/app.module.ts
+++ src/AngularClientCode/angularApp/app/app.module.ts
@@ -26,7 +26,6 @@ import { AuthorizationGuard } from './authorization.guard';
 import { AuthorizationCanGuard } from './authorization.can.guard';
 
 import { OidcConfigService } from './auth/services/oidc.security.config.service';
-import { AuthWellKnownEndpoints } from './auth/models/auth.well-known-endpoints';
 
 const l10nConfig: L10nConfig = {
     locale: {
@@ -99,44 +98,30 @@ export class AppModule {
 
         this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
 
-            const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
-            openIDImplicitFlowConfiguration.stsServer = this.oidcConfigService.clientConfiguration.stsServer;
-            openIDImplicitFlowConfiguration.redirect_url = this.oidcConfigService.clientConfiguration.redirect_url;
-            // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer
-            // identified by the iss (issuer) Claim as an audience.
-            // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience,
-            // or if it contains additional audiences not trusted by the Client.
-            openIDImplicitFlowConfiguration.client_id = this.oidcConfigService.clientConfiguration.client_id;
-            openIDImplicitFlowConfiguration.response_type = this.oidcConfigService.clientConfiguration.response_type;
-            openIDImplicitFlowConfiguration.scope = this.oidcConfigService.clientConfiguration.scope;
-            openIDImplicitFlowConfiguration.post_logout_redirect_uri = this.oidcConfigService.clientConfiguration.post_logout_redirect_uri;
-            openIDImplicitFlowConfiguration.start_checksession = this.oidcConfigService.clientConfiguration.start_checksession;
-
-            openIDImplicitFlowConfiguration.silent_renew = this.oidcConfigService.clientConfiguration.silent_renew;
-            openIDImplicitFlowConfiguration.silent_renew_url = this.oidcConfigService.clientConfiguration.redirect_url + '/silent-renew.html';
-
-            openIDImplicitFlowConfiguration.post_login_route = this.oidcConfigService.clientConfiguration.startup_route;
-            // HTTP 403
-            openIDImplicitFlowConfiguration.forbidden_route = this.oidcConfigService.clientConfiguration.forbidden_route;
-            // HTTP 401
-            openIDImplicitFlowConfiguration.unauthorized_route = this.oidcConfigService.clientConfiguration.unauthorized_route;
-            openIDImplicitFlowConfiguration.log_console_warning_active = this.oidcConfigService.clientConfiguration.log_console_warning_active;
-            openIDImplicitFlowConfiguration.log_console_debug_active = this.oidcConfigService.clientConfiguration.log_console_debug_active;
-            // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
-            // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
-            openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds =
+            const config = new OpenIDImplicitFlowConfiguration();
+            config.stsServer = this.oidcConfigService.clientConfiguration.stsServer;
+            config.redirect_url = this.oidcConfigService.clientConfiguration.redirect_url;
+            config.client_id = this.oidcConfigService.clientConfiguration.client_id;
+            config.response_type = this.oidcConfigService.clientConfiguration.response_type;
+            config.scope = this.oidcConfigService.clientConfiguration.scope;
+            config.post_logout_redirect_uri = this.oidcConfigService.clientConfiguration.post_logout_redirect_uri;
+            config.start_checksession = this.oidcConfigService.clientConfiguration.start_checksession;
+            config.silent_renew = this.oidcConfigService.clientConfiguration.silent_renew;
+            config.silent_renew_url = this.oidcConfigService.clientConfiguration.redirect_url + '/silent-renew.html';
+            config.post_login_route = this.oidcConfigService.clientConfiguration.startup_route;
+            config.forbidden_route = this.oidcConfigService.clientConfiguration.forbidden_route;
+            config.unauthorized_route = this.oidcConfigService.clientConfiguration.unauthorized_route;
+            config.log_console_warning_active = this.oidcConfigService.clientConfiguration.log_console_warning_active;
+            config.log_console_debug_active = this.oidcConfigService.clientConfiguration.log_console_debug_active;
+            config.max_id_token_iat_offset_allowed_in_seconds =
                 this.oidcConfigService.clientConfiguration.max_id_token_iat_offset_allowed_in_seconds;
+            config.history_cleanup_off = true;
+            // config.iss_validation_off = false;
 
-            // openIDImplicitFlowConfiguration.iss_validation_off = false;
             configuration.FileServer = this.oidcConfigService.clientConfiguration.apiFileServer;
             configuration.Server = this.oidcConfigService.clientConfiguration.apiServer;
 
-            openIDImplicitFlowConfiguration.history_cleanup_off = true;
-
-            const authWellKnownEndpoints = new AuthWellKnownEndpoints();
-            authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
-
-            this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
+            this.oidcSecurityService.setupModule(config, this.oidcConfigService.wellKnownEndpoints);
 
         });
 
