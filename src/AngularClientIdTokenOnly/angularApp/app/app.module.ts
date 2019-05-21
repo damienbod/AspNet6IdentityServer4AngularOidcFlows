369595874792e26cf239da701f8c7b0354190d25
damienbod <damien_bod@hotmail.com>
2019-05-21 22:36:20 +0200

    fixing implicit flow login

--- src/AngularClientIdTokenOnly/angularApp/app/app.module.ts
+++ src/AngularClientIdTokenOnly/angularApp/app/app.module.ts
@@ -83,7 +83,7 @@ export class AppModule {
             // HTTP 401
             openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
             openIDImplicitFlowConfiguration.log_console_warning_active = true;
-            openIDImplicitFlowConfiguration.log_console_debug_active = false;
+            openIDImplicitFlowConfiguration.log_console_debug_active = true;
             // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
             // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
             openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 3;
