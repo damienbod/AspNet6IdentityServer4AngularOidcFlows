34d590e13aa5bf24f0936d45663e6986869847a2
damienbod <damien_bod@hotmail.com>
2019-05-24 14:50:09 +0200

    updating configuration names

--- src/AngularClient/angularApp/app/auth/services/oidc.security.service.ts
+++ src/AngularClient/angularApp/app/auth/services/oidc.security.service.ts
@@ -372,7 +372,7 @@ export class OidcSecurityService {
             data =
                 `grant_type=authorization_code&client_id=${this.configurationProvider.openIDConfiguration.client_id}` +
                 `&code_verifier=${this.oidcSecurityCommon.code_verifier}&code=${code}&redirect_uri=${
-            this.configurationProvider.openIDConfiguration.silent_renew_url
+                    this.configurationProvider.openIDConfiguration.silent_renew_url
                 }`;
         }
 
