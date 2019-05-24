4d5c6b6a4ddfed3ef3053801aa3f39716299ed48
damienbod <damien_bod@hotmail.com>
2019-05-24 14:48:25 +0200

    code clean up

--- src/AngularClientCode/angularApp/app/auth/modules/auth.configuration.ts
+++ src/AngularClientCode/angularApp/app/auth/modules/auth.configuration.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { Observable, Subject } from 'rxjs';
 
-export class OpenIDImplicitFlowConfiguration {
+export class OpenIDConfiguration {
     stsServer = 'https://localhost:44318';
     redirect_url = 'https://localhost:44311';
     // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified
@@ -42,68 +42,68 @@ export class OpenIDImplicitFlowConfiguration {
 
 @Injectable()
 export class AuthConfiguration {
-    private openIDImplicitFlowConfiguration: OpenIDImplicitFlowConfiguration | undefined;
-    private defaultConfig: OpenIDImplicitFlowConfiguration;
+    private openIDConfiguration: OpenIDConfiguration | undefined;
+    private defaultConfig: OpenIDConfiguration;
 
     get stsServer(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.stsServer;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.stsServer;
         }
 
         return this.defaultConfig.stsServer;
     }
 
     get redirect_url(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.redirect_url;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.redirect_url;
         }
 
         return this.defaultConfig.redirect_url;
     }
 
     get silent_renew_url(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.silent_renew_url;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.silent_renew_url;
         }
 
         return this.defaultConfig.silent_renew_url;
     }
 
     get client_id(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.client_id;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.client_id;
         }
 
         return this.defaultConfig.client_id;
     }
 
     get response_type(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.response_type;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.response_type;
         }
 
         return this.defaultConfig.response_type;
     }
 
     get scope(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.scope;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.scope;
         }
 
         return this.defaultConfig.scope;
     }
 
     get hd_param(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.hd_param;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.hd_param;
         }
 
         return this.defaultConfig.hd_param;
     }
 
     get post_logout_redirect_uri(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.post_logout_redirect_uri;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.post_logout_redirect_uri;
         }
 
         return this.defaultConfig.post_logout_redirect_uri;
@@ -114,8 +114,8 @@ export class AuthConfiguration {
             return false;
         }
 
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.start_checksession;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.start_checksession;
         }
 
         return this.defaultConfig.start_checksession;
@@ -126,136 +126,136 @@ export class AuthConfiguration {
             return false;
         }
 
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.silent_renew;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.silent_renew;
         }
 
         return this.defaultConfig.silent_renew;
     }
 
     get silent_renew_offset_in_seconds(): number {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.silent_renew_offset_in_seconds;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.silent_renew_offset_in_seconds;
         }
 
         return this.defaultConfig.silent_renew_offset_in_seconds;
     }
 
     get post_login_route(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.post_login_route;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.post_login_route;
         }
 
         return this.defaultConfig.post_login_route;
     }
 
     get forbidden_route(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.forbidden_route;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.forbidden_route;
         }
 
         return this.defaultConfig.forbidden_route;
     }
 
     get unauthorized_route(): string {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.unauthorized_route;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.unauthorized_route;
         }
 
         return this.defaultConfig.unauthorized_route;
     }
 
     get auto_userinfo(): boolean {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.auto_userinfo;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.auto_userinfo;
         }
 
         return this.defaultConfig.auto_userinfo;
     }
 
     get auto_clean_state_after_authentication(): boolean {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.auto_clean_state_after_authentication;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.auto_clean_state_after_authentication;
         }
 
         return this.defaultConfig.auto_clean_state_after_authentication;
     }
 
     get trigger_authorization_result_event(): boolean {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.trigger_authorization_result_event;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.trigger_authorization_result_event;
         }
 
         return this.defaultConfig.trigger_authorization_result_event;
     }
 
     get isLogLevelWarningEnabled(): boolean {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.log_console_warning_active;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.log_console_warning_active;
         }
 
         return this.defaultConfig.log_console_warning_active;
     }
 
     get isLogLevelDebugEnabled(): boolean {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.log_console_debug_active;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.log_console_debug_active;
         }
 
         return this.defaultConfig.log_console_debug_active;
     }
 
     get iss_validation_off(): boolean {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.iss_validation_off;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.iss_validation_off;
         }
 
         return this.defaultConfig.iss_validation_off;
     }
 
     get history_cleanup_off(): boolean {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.history_cleanup_off;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.history_cleanup_off;
         }
 
         return this.defaultConfig.history_cleanup_off;
     }
 
     get max_id_token_iat_offset_allowed_in_seconds(): number {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.max_id_token_iat_offset_allowed_in_seconds;
         }
 
         return this.defaultConfig.max_id_token_iat_offset_allowed_in_seconds;
     }
 
     get disable_iat_offset_validation(): boolean {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.disable_iat_offset_validation;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.disable_iat_offset_validation;
         }
 
         return this.defaultConfig.disable_iat_offset_validation;
     }
 
     get storage(): any {
-        if (this.openIDImplicitFlowConfiguration) {
-            return this.openIDImplicitFlowConfiguration.storage;
+        if (this.openIDConfiguration) {
+            return this.openIDConfiguration.storage;
         }
 
         return this.defaultConfig.storage;
     }
 
     constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-        this.defaultConfig = new OpenIDImplicitFlowConfiguration();
+        this.defaultConfig = new OpenIDConfiguration();
     }
 
-    init(openIDImplicitFlowConfiguration: OpenIDImplicitFlowConfiguration) {
-        this.openIDImplicitFlowConfiguration = openIDImplicitFlowConfiguration;
-        this._onConfigurationChange.next(openIDImplicitFlowConfiguration);
+    init(openIDConfiguration: OpenIDConfiguration) {
+        this.openIDConfiguration = openIDConfiguration;
+        this._onConfigurationChange.next(openIDConfiguration);
     }
 
-    private _onConfigurationChange = new Subject<OpenIDImplicitFlowConfiguration>();
-    get onConfigurationChange(): Observable<OpenIDImplicitFlowConfiguration> {
+    private _onConfigurationChange = new Subject<OpenIDConfiguration>();
+    get onConfigurationChange(): Observable<OpenIDConfiguration> {
         return this._onConfigurationChange.asObservable();
     }
 }
