eab1cb598d5223a1532d14f9b7bb60bf0672e933
damienbod <damien_bod@hotmail.com>
2019-06-10 18:48:43 +0200

    renaming authWellknownEndpoints

--- src/AngularClient/angularApp/app/app.module.ts
+++ src/AngularClient/angularApp/app/app.module.ts
@@ -119,49 +119,8 @@ export class AppModule {
             configuration.FileServer = configResult.customConfig.apiFileServer;
             configuration.Server = configResult.customConfig.apiServer;
 
-            this.oidcSecurityService.setupModule(config, configResult.customAuthWellknownEndpoints);
+            this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
         });
-
-        // this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
-
-        //    const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
-        //    openIDImplicitFlowConfiguration.stsServer = this.oidcConfigService.clientConfiguration.stsServer;
-        //    openIDImplicitFlowConfiguration.redirect_url = this.oidcConfigService.clientConfiguration.redirect_url;
-        //    // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer
-        //    // identified by the iss (issuer) Claim as an audience.
-        //    // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience,
-        //    // or if it contains additional audiences not trusted by the Client.
-        //    openIDImplicitFlowConfiguration.client_id = this.oidcConfigService.clientConfiguration.client_id;
-        //    openIDImplicitFlowConfiguration.response_type = this.oidcConfigService.clientConfiguration.response_type;
-        //    openIDImplicitFlowConfiguration.scope = this.oidcConfigService.clientConfiguration.scope;
-        //    openIDImplicitFlowConfiguration.post_logout_redirect_uri = this.oidcConfigService.clientConfiguration.post_logout_redirect_uri;
-        //    openIDImplicitFlowConfiguration.start_checksession = this.oidcConfigService.clientConfiguration.start_checksession;
-
-        //    openIDImplicitFlowConfiguration.silent_renew = this.oidcConfigService.clientConfiguration.silent_renew;
-        //    openIDImplicitFlowConfiguration.silent_renew_url = 'https://localhost:44311/silent-renew.html';
-
-        //    openIDImplicitFlowConfiguration.post_login_route = this.oidcConfigService.clientConfiguration.startup_route;
-        //    // HTTP 403
-        //    openIDImplicitFlowConfiguration.forbidden_route = this.oidcConfigService.clientConfiguration.forbidden_route;
-        //    // HTTP 401
-        //    openIDImplicitFlowConfiguration.unauthorized_route = this.oidcConfigService.clientConfiguration.unauthorized_route;
-        //    openIDImplicitFlowConfiguration.log_console_warning_active = this.oidcConfigService.clientConfiguration.log_console_warning_active;
-        //    openIDImplicitFlowConfiguration.log_console_debug_active = this.oidcConfigService.clientConfiguration.log_console_debug_active;
-        //    // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
-        //    // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
-        //    openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds =
-        //        this.oidcConfigService.clientConfiguration.max_id_token_iat_offset_allowed_in_seconds;
-
-        //    // openIDImplicitFlowConfiguration.iss_validation_off = false;
-        //    configuration.FileServer = this.oidcConfigService.clientConfiguration.apiFileServer;
-        //    configuration.Server = this.oidcConfigService.clientConfiguration.apiServer;
-
-        //    const authWellKnownEndpoints = new AuthWellKnownEndpoints();
-        //    authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
-
-        //    this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
-
-        // });
 
         console.log('APP STARTING');
     }
--- src/AngularClientCode/angularApp/app/app.module.ts
+++ src/AngularClientCode/angularApp/app/app.module.ts
@@ -93,28 +93,6 @@ export class AppModule {
     ) {
         this.l10nLoader.load();
 
-        // this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
-        //    const config: OpenIdConfiguration = {
-        //        stsServer: configResult.customConfig.stsServer,
-        //        redirect_url: 'https://localhost:4200',
-        //        client_id: 'angularClient',
-        //        scope: 'openid profile email',
-        //        response_type: 'code',
-        //        silent_renew: true,
-        //        silent_renew_url: 'https://localhost:4200/silent-renew.html',
-        //        log_console_debug_active: true,
-        //    };
-
-        //    //config.start_checksession = true;
-        //    //config.post_login_route = '/home';
-        //    //config.forbidden_route = '/home';
-        //    //config.unauthorized_route = '/home';
-        //    //config.max_id_token_iat_offset_allowed_in_seconds = 5;
-        //    //config.history_cleanup_off = true;
-
-        //    this.oidcSecurityService.setupModule(config, configResult.customAuthWellknownEndpoints);
-        // });
-
         this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
 
             const config: OpenIdConfiguration = {
@@ -141,7 +119,7 @@ export class AppModule {
             configuration.FileServer = configResult.customConfig.apiFileServer;
             configuration.Server = configResult.customConfig.apiServer;
 
-            this.oidcSecurityService.setupModule(config, configResult.customAuthWellknownEndpoints);
+            this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
         });
 
         console.log('APP STARTING');
