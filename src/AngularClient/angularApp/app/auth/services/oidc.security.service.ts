82a4aeebf61701672ac5c8e14287f21b66e4f45f
damienbod <damien_bod@hotmail.com>
2019-09-05 10:22:08 +0200

    updating URL parsing

--- src/AngularClient/angularApp/app/auth/services/oidc.security.service.ts
+++ src/AngularClient/angularApp/app/auth/services/oidc.security.service.ts
@@ -21,6 +21,7 @@ import { OidcSecuritySilentRenew } from './oidc.security.silent-renew';
 import { OidcSecurityUserService } from './oidc.security.user-service';
 import { OidcSecurityValidation } from './oidc.security.validation';
 import { UriEncoder } from './uri-encoder';
+import { UrlParserService } from './url-parser.service';
 
 @Injectable()
 export class OidcSecurityService {
@@ -71,7 +72,8 @@ export class OidcSecurityService {
         private loggerService: LoggerService,
         private zone: NgZone,
         private readonly httpClient: HttpClient,
-        private readonly configurationProvider: ConfigurationProvider
+        private readonly configurationProvider: ConfigurationProvider,
+        private readonly urlParserService: UrlParserService,
     ) {
         this.onModuleSetup.pipe(take(1)).subscribe(() => {
             this.moduleSetup = true;
@@ -331,34 +333,30 @@ export class OidcSecurityService {
 
     // Code Flow
     authorizedCallbackWithCode(urlToCheck: string) {
-        const urlParts = urlToCheck.split('?');
-        const params = new HttpParams({
-            fromString: urlParts[1],
-        });
-        const code = params.get('code');
-        const state = params.get('state');
-        const session_state = params.get('session_state');
+        const code = this.urlParserService.getUrlParameter(urlToCheck, 'code');
+        const state = this.urlParserService.getUrlParameter(urlToCheck, 'state');
+        const sessionState = this.urlParserService.getUrlParameter(urlToCheck, 'session_state') || null;
 
-        if (code && state) {
-            this.requestTokensWithCode(code, state, session_state);
+        if (!!code && !!state) {
+            this.requestTokensWithCode(code, state, sessionState);
         }
     }
 
     // Code Flow
-    requestTokensWithCode(code: string, state: string, session_state: string | null) {
+    requestTokensWithCode(code: string, state: string, sessionState: string | null) {
         this._isModuleSetup
             .pipe(
                 filter((isModuleSetup: boolean) => isModuleSetup),
                 take(1)
             )
             .subscribe(() => {
-                this.requestTokensWithCodeProcedure(code, state, session_state);
+                this.requestTokensWithCodeProcedure(code, state, sessionState);
             });
     }
 
     // Refresh Token
     refreshTokensWithCodeProcedure(code: string, state: string): Observable<any> {
-        let tokenRequestUrl = '';
+        let tokenRequestUrl  = '';
         if (this.configurationProvider.wellKnownEndpoints && this.configurationProvider.wellKnownEndpoints.token_endpoint) {
             tokenRequestUrl = `${this.configurationProvider.wellKnownEndpoints.token_endpoint}`;
         }
@@ -693,15 +691,16 @@ export class OidcSecurityService {
 
         // Code Flow
         if (this.configurationProvider.openIDConfiguration.response_type === 'code') {
-            // try using refresh token
-            const refresh_token = this.oidcSecurityCommon.getRefreshToken();
-            if (refresh_token) {
-                this.loggerService.logDebug('found refresh code, obtaining new credentials with refresh code');
-                return this.refreshTokensWithCodeProcedure(refresh_token, state);
-            } else {
-                this.loggerService.logDebug('no refresh token found, using silent renew');
+            if (this.configurationProvider.openIDConfiguration.use_refresh_token) {
+                // try using refresh token
+                const refresh_token = this.oidcSecurityCommon.getRefreshToken();
+                if (refresh_token) {
+                    this.loggerService.logDebug('found refresh code, obtaining new credentials with refresh code');
+                    return this.refreshTokensWithCodeProcedure(refresh_token, state);
+                } else {
+                    this.loggerService.logDebug('no refresh token found, using silent renew');
+                }
             }
-
             // code_challenge with "S256"
             const code_verifier = 'C' + Math.random() + '' + Date.now() + '' + Date.now() + Math.random();
             const code_challenge = this.oidcSecurityValidation.generate_code_verifier(code_verifier);
