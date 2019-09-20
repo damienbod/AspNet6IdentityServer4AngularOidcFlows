2da52cb2e3548f0b2d8a03f25304abd97dc3644a
damienbod <damien_bod@hotmail.com>
2019-09-20 12:54:58 +0200

    fixing angular code

---
+++ src/AngularClientIdTokenOnly/angularApp/app/dataeventrecords/dataeventrecords.routes.ts
@@ -0,0 +1,22 @@
+import { Routes, RouterModule } from '@angular/router';
+import { DataEventRecordsListComponent } from './components/dataeventrecords-list.component';
+import { DataEventRecordsCreateComponent } from './components/dataeventrecords-create.component';
+import { DataEventRecordsEditComponent } from './components/dataeventrecords-edit.component';
+
+
+const routes: Routes = [
+    {
+        path: 'dataeventrecords/create',
+        component: DataEventRecordsCreateComponent
+    },
+    {
+        path: 'dataeventrecords/edit/:id',
+        component: DataEventRecordsEditComponent
+    },
+    {
+        path: 'dataeventrecords',
+        component: DataEventRecordsListComponent,
+    }
+];
+
+export const DataEventRecordsRoutes = RouterModule.forChild(routes);
