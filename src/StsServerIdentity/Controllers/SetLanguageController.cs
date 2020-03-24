729c570e477ba7d2384c61b71ae6780d0575cf7a
damienbod <damien_bod@hotmail.com>
2020-03-24 07:44:35 +0100

    updating language cookie

--- src/StsServerIdentity/Controllers/SetLanguageController.cs
+++ src/StsServerIdentity/Controllers/SetLanguageController.cs
@@ -18,7 +18,7 @@ namespace StsServerIdentity.Controllers
             Response.Cookies.Append(
                 CookieRequestCultureProvider.DefaultCookieName,
                 CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
-                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1), Secure = true, SameSite = SameSiteMode.None }
             );
 
             return Redirect(returnUrlForLanguageSwitch);
