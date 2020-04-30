c6c421af01db6e3295ad55565b01b77da9a47b4f
damienbod <damien_bod@hotmail.com>
2020-04-30 22:21:35 +0200

    updating lib to Version 11

---
+++ src/AngularClientCode/angularApp/app/auth/angular-auth-oidc-client.ts
@@ -0,0 +1,19 @@
+// Public classes.
+
+export * from './auth.module';
+export * from './authState/authorization-result';
+export * from './authState/authorized-state';
+export * from './config/auth-well-known-endpoints';
+export * from './config/config.service';
+export * from './config/openid-configuration';
+export * from './config/public-configuration';
+export * from './logging/log-level';
+export * from './logging/logger.service';
+export * from './oidc.security.service';
+export * from './public-events';
+export * from './storage';
+export * from './utils/tokenHelper/oidc-token-helper.service';
+export * from './validation/jwtkeys';
+export * from './validation/state-validation-result';
+export * from './validation/token-validation.service';
+export * from './validation/validation-result';
