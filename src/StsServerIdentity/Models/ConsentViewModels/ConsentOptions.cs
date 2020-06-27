d9ae02f33e1535aa238ab825b0e7eb3e815732f0
damienbod <damien_bod@hotmail.com>
2020-06-27 09:59:44 +0200

    code clean up

--- src/StsServerIdentity/Models/ConsentViewModels/ConsentOptions.cs
+++ src/StsServerIdentity/Models/ConsentViewModels/ConsentOptions.cs
@@ -1,8 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-namespace StsServerIdentity.Models
+﻿namespace StsServerIdentity.Models
 {
     public class ConsentOptions
     {
